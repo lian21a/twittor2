035f55898d1730cd0dba304dfb0f4d6875d9e623
lian21a <153049987+lian21a@users.noreply.github.com>
2023-12-06 00:39:54 -0600

    Add files via upload
    Actualización

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var swLocation='/twittor/sw.js'
 
 if (navigator.serviceWorker){
     if(url.includes('localhost')){
-        swLocation='/sw-utils.js';
+        swLocation='/sw.js';
     }
     navigator.serviceWorker.register(swLocation);
 }
